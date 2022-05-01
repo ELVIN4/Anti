14945cdbcd9afce8f1f11f481093895025080dc2
ELVIN <71011947+ELVIN4@users.noreply.github.com>
2022-05-02 00:09:39 +0200

    Update data.py

--- data.py
+++ data.py
@@ -16,17 +16,78 @@ mails = (
 )
 
 names = [
-    'Showy', 'Andrey', 'Sasha', 'Masha','Petya', 'Vasya', 'Sonya', 'Evgeniy', 'Hykok', 'What', 'Youn', 'Iewa', 'Rooti'
-    'Good', 'Jask', 'Luka', 'Artem', 'Maga' 'Fifi', 'Fufu', 'Lolik', 'Tolik', 'Petz', 'Hsdf', 'Pope', 'None', 'Noud', 'Urukbek',
-    'James', 'Jony', 'Jacob', 'Cristi', 'Benjamin', 'Gregory', 'Caesar', 'Tony', 'Ara', 'Harry', 'Garry', 'Larry', 'Nixon', 'Kanny',
+    "Showy",
+    "Andrey",
+    "Sasha",
+    "Masha",
+    "Petya",
+    "Vasya",
+    "Sonya",
+    "Evgeniy",
+    "Hykok",
+    "What",
+    "Youn",
+    "Iewa",
+    "Rooti" "Good",
+    "Jask",
+    "Luka",
+    "Artem",
+    "Maga" "Fifi",
+    "Fufu",
+    "Lolik",
+    "Tolik",
+    "Petz",
+    "Hsdf",
+    "Pope",
+    "None",
+    "Noud",
+    "Urukbek",
+    "James",
+    "Jony",
+    "Jacob",
+    "Cristi",
+    "Benjamin",
+    "Gregory",
+    "Caesar",
+    "Tony",
+    "Ara",
+    "Harry",
+    "Garry",
+    "Larry",
+    "Nixon",
+    "Kanny",
 ]
 
 ru_name = [
-    'Степа', 'Ксюша', 'Петр','Иван','Степан','Магомед','Мага', 'Люба', 'Маша', 'Паша', 'Ваня', 'Саша', 'Женя', 'Евгения', 'София', 
-    'Арсений', 'Василий', 'Максим', 'Грегорий', 'Нурлан', 'Артем', 'Анастасия', 'Женя', 'Катя', 'Карина', 'Аня',
+    "Степа",
+    "Ксюша",
+    "Петр",
+    "Иван",
+    "Степан",
+    "Магомед",
+    "Мага",
+    "Люба",
+    "Маша",
+    "Паша",
+    "Ваня",
+    "Саша",
+    "Женя",
+    "Евгения",
+    "София",
+    "Арсений",
+    "Василий",
+    "Максим",
+    "Грегорий",
+    "Нурлан",
+    "Артем",
+    "Анастасия",
+    "Женя",
+    "Катя",
+    "Карина",
+    "Аня",
 ]
 
-agent = open('user_agents.txt').read().split('\n')
+agent = open("user_agents.txt").read().split("\n")
 
 
 def name():
@@ -34,12 +95,13 @@ def name():
 
 
 def username():
-    data_user = ''
-    letters = 'wertyuiopbvcsdfghjklkjewfvgbnxcfghuiasdfgh'
+    data_user = ""
+    letters = "wertyuiopbvcsdfghjklkjewfvgbnxcfghuiasdfgh"
     for new_user in letters[:8]:
-        data_user+=data_user.join(random.choice(list(letters)))
+        data_user += data_user.join(random.choice(list(letters)))
 
     return name() + data_user
+
 
 def x(int_range=4):
     numbers = []
@@ -47,14 +109,14 @@ def x(int_range=4):
         numbers.append(str(random.randint(1, 9)))
     return "".join(numbers)
 
+
 def email():
     return name() + x() + "@" + random.choice(mails)
+
 
 def password():
     return name() + x(int_range=9)
 
+
 def user_agent():
     return random.choice(agent)
-
-
-
